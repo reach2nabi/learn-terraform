9a0ba742b63a865f8130d639a4c9704676854bc3
rasoolasn <rasoolasn@gmail.com>
2023-08-12 10:59:06 +0530

    terraform

--- roboshop.v3/main.tf
+++ roboshop.v3/main.tf
@@ -42,6 +42,6 @@ resource "aws_route53_record" "records" {
   name    = "${lookup(each.value,"name",null)}.ndevops.online"
   type    = "A"
   ttl     =  30
-  records = [lookup(lookup(aws_instance.instance, each.key,"null") "private_ip",null)]
+  records = [lookup(lookup(aws_instance.instance, each.key,"null"), "private_ip",null)]
 }
 
